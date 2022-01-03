dfd5df498114d84dbaf7beb50ffae9485c7dfa4d
eunyupp kim <fubugid12@naver.com>
2022-01-03 23:54:56 +0900

    Initial Commit

---
+++ QuotesGenerator/Quote.swift
@@ -0,0 +1,15 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by EunYupp Kim on 2022/01/03.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+        
+    
+}
